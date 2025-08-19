ce4ab66bd4071c3548447c7b7034b86a0842aa52
Gabby291 <gabby.t0130@gmail.com>
2025-08-18 21:57:36 -0400

    booking

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const API_URL = 'http://localhost:5000/api'; // or your backend URL
+export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+console.log(
+    "API_URL :",
+    API_URL
+);
